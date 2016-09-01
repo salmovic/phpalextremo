e782cf0241282f9a530fb7df37f148a5ea4cf6e1
Aquiles <mv.salvador93@gmail.com>
2016-08-31 19:02:53 -0500

    Video3
    Instalacion de composer e instalando paypal y  phpmailer

---
+++ php_avanzado/core/core.php
@@ -0,0 +1,16 @@
+<?php
+/*
+NUCLEO DE LA APLICACION
+*/
+
+// Se definen las constantes
+// root
+define('HTML_DIR','html/');
+// Nombre de la aplicacion
+define('APP_TITLE','OcrendBB Hol');
+// Pie de pagina
+define( 'FOOTER_COPY', 'Copy &copy; '.date( 'Y' ).' Salvador' );
+//cargar todas las librerias... [vendor]
+require('vendor/autoload.php');
+
+ ?>
