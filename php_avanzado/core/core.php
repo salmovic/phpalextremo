95fcb3f437d9cc338011790b1c5c205fce4c5c0a
Aquiles <mv.salvador93@gmail.com>
2016-09-02 21:48:34 -0500

    Add Laravel5.2
    Agregando los repositorio de Laravel en su version 5.2

--- php_avanzado/core/core.php
+++ php_avanzado/core/core.php
@@ -10,7 +10,11 @@ define('HTML_DIR','html/');
 define('APP_TITLE','OcrendBB Hol');
 // Pie de pagina
 define( 'FOOTER_COPY', 'Copy &copy; '.date( 'Y' ).' Salvador' );
+//url por defecto
+define('APP_URL', 'http://localhost/phpalextremo/php_avanzado/');
+
 //cargar todas las librerias... [vendor]
 require('vendor/autoload.php');
+
 
  ?>
--- php_avanzado/html/overall/header.php
+++ php_avanzado/html/overall/header.php
@@ -2,6 +2,8 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
+	<!-- Url de base -->
+	<base href="<?php echo APP_URL; ?>" target="_blank" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="generator" content="Mobirise v2.8.3, mobirise.com">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,8 +15,12 @@
   <link rel="stylesheet" href="views/socicon/css/socicon.min.css">
   <link rel="stylesheet" href="views/mobirise/css/style.css">
   <link rel="stylesheet" href="views/mobirise/css/mbr-additional.css" type="text/css">
-  <link rel="stylesheet" href="views/fontawesome/css/font.css" type="text/css">
+  <link rel="stylesheet" href="views/fontawesome/css/fonts.css" type="text/css">
 
+<!-- Mis funciones js -->
+<script src="views/app/js/generales.js"></script>
+
+</script>
   <title><?php echo APP_TITLE; ?></title>
 
 </head>
